94bfdccc053e14afe7cc305935c8816ec1120247
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-07 09:21:35 -0700

    Added bootstrap templates and routes to server.py

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 app.secret_key = "Supermegasecret"
 
+@app.route('/')
+def index():
+	return render_template('index.html')
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/post')
+def post():
+	return render_template('post.html')
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
 
 if name == "main":
 	app.run(port=5000, host='0.0.0.0')
