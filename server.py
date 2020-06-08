a8a7e6994a3b9826b792b87dfec7ca5c71b30fd1
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-08 10:20:32 -0700

    Formatted datetime to show pretty date

--- server.py
+++ server.py
@@ -30,7 +30,9 @@ def about():
 @app.route('/post/<int:post_id>')
 def post(post_id):
 	post = Blogpost.query.filter_by(id=post_id).one()
-	return render_template('post.html', post=post)
+
+	date_posted = post.date.strftime('%B %d, %Y')
+	return render_template('post.html', post=post, date_posted=date_posted)
 
 @app.route('/contact')
 def contact():
